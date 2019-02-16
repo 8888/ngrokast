669cc39a8f142852a9cab3e63a7cc9574c1ae311
betterin30days <lee@rubiconmd.com>
2019-02-16 11:47:35 -0500

    code consolidation, roundtrip works correctly

--- broadcast/handler.js
+++ broadcast/handler.js
@@ -1,42 +1,43 @@
 'use strict';
 
-const s3 = require('./awsS3');
+const aws = require('aws-sdk');
+const secrets = require('../secrets.json');
 
 module.exports.broadcast = (event, context, callback) => {
-  let url,
-      statusCode,
-      body;
+  let response = {headers:{'Access-Control-Allow-Origin': '*'}};
+  let url;
 
   try {
+    // http request
     const payload = JSON.parse(event.body);
     url = payload.url;
   }
   catch(error) {
+    // local invocation or through aws console
     url = event.url;
   }
 
   if (url) {
-    try {
-      s3.upload(url);
-      statusCode = 200;
-      body = JSON.stringify({url, message: 'url successfully broadcasted'});
-    }
-    catch(error) {
-      statusCode = 500;
-      body = JSON.stringify({url, message: 'upload to S3 failed'});
-    }
+    const s3 = new aws.S3();
+    const params = {
+      Body: url,
+      Bucket: secrets.bucket,
+      Key: 'url'
+    };
+    s3.putObject(params, (err, data) => {
+      if (err) {
+        response.statusCode = 500;
+        response.body = JSON.stringify({url, message: 'upload to S3 failed'});
+      }
+      else {
+        response.statusCode = 200;
+        response.body = JSON.stringify({url, message: 'url successfully broadcasted'});
+      }
+      callback(null, response);
+    });
   } else {
-    statusCode = 400;
-    body = JSON.stringify({event, message: 'no url provided'});
+    response.statusCode = 400;
+    response.body = JSON.stringify({event, message: 'no url provided'});
+    callback(null, response);
   }
-
-  const response = {
-    statusCode,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    body,
-  };
-
-  callback(null, response);
 };
--- ngrokast.sh
+++ ngrokast.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-source secrets.sh
+source secrets.sh # or full path if aliased
 URL=$(curl http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url")
 curl -H "x-api-key: $API_KEY" -H "Content-Type: application/json" -d '{"url":'$URL'}' $API_URL
